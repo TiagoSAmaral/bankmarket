c9dfc537b7d615f53c6e1f37cf5e9429654c98dc
Tiago Amaral <tiagofly@outlook.com>
2023-06-18 02:58:39 -0300

    Update LayoutCardView.swift Implemetnado enum de layout para cards

---
+++ source/app/Shared/Models/LayoutCardView.swift
@@ -0,0 +1,14 @@
+//
+//  LayoutCardView.swift
+//  BaseProjectTarget
+//
+//  Created by Tiago Amaral on 18/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import Foundation
+
+enum CardLayoutView {
+    case cardListItemView
+    case cardDetailItemView
+}
