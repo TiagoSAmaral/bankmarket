07177fd1d26b3a5aee4a6e15d7e152ec76f5481e
Tiago Amaral <tiagofly@outlook.com>
2023-06-18 03:04:12 -0300

    Update CardFactory.swift Contrutor de view dos cards conforme o layout indicado na propriedade da interface Visible do model.

---
+++ source/app/Shared/Factorys/CardFactory.swift
@@ -0,0 +1,37 @@
+//
+//  CardFactory.swift
+//  BaseProjectTarget
+//
+//  Created by Tiago Amaral on 18/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import Foundation
+
+protocol CardFactory {
+    func makeCard(from item: Visible?) -> CardVisible?
+}
+
+final class CardConstructor: CardFactory {
+    
+    func makeCard(from item: Visible?) -> CardVisible? {
+        guard let item = item else {
+            return nil
+        }
+        
+        switch item.layoutView {
+        case .cardListItemView:
+            return CardConstructor.make(with: item, classType: CardListItemView.self)
+        case .cardDetailItemView:
+            return CardConstructor.make(with: item, classType: CardDetailItemView.self)
+        default:
+            return nil
+        }
+    }
+    
+    static func make<T: CardVisible>(with data: Model, classType: T.Type) -> T {
+        let cardView = T.init()
+        cardView.load(model: data)
+        return cardView
+    }
+}
