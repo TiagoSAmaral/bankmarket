fb71fc259021ebbf7a2c482983e5387c918510b2
Tiago Amaral <tiagofly@outlook.com>
2023-06-18 03:07:17 -0300

    Update MosaicBaseView.swift Uma base view para ajudar a compor elementos de tela usando um Stackview de base principal.

---
+++ source/app/Shared/Components/MosaicBaseView/MosaicBaseView.swift
@@ -0,0 +1,37 @@
+//
+//  MosaicBaseView.swift
+//  BaseProjectTarget
+//
+//  Created by Tiago Amaral on 18/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import UIKit
+
+class MosaicBaseView {
+    
+    lazy var baseView: UIView = {
+        var view = UIView()
+        view.backgroundColor = .systemBackground // ColorAsset.cardBackgroundColor
+        return view
+    }()
+    
+    lazy var vStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    init() {
+        makeConstraint()
+    }
+    
+    func insertNew(view: UIView) {
+        vStackView.addArrangedSubview(view)
+    }
+    
+    func makeConstraint() {
+        baseView.addSubviews([vStackView])
+        vStackView.edgeToSuperView()
+    }
+}
