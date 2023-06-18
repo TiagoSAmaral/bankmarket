718d44480a02f1ac8ab3364bc6f0ac4970c5d4b8
Tiago Amaral <tiagofly@outlook.com>
2023-06-18 02:58:59 -0300

    Update Item.swift implemetnado model com principais informacoes dos dados a serem exibidos.

---
+++ source/app/Shared/Models/Item.swift
@@ -0,0 +1,30 @@
+//
+//  HSCard.swift
+//  BaseProjectTarget
+//
+//  Created by Tiago Amaral on 16/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import UIKit
+
+struct Item: Decodable {
+    let id: Int?
+    let slug: String?
+
+    let image: String?
+    let name: String?
+    let flavorText: String?
+    let text: String?   // property text conatins HTML tags. Must be handled with NSAttributedString
+    let cardSetId: Int? // set pertencente
+    let cardTypeId: Int? // type
+                         // Como associar a facção
+    let rarityId: Int?
+    let attack: Int?
+    let manaCost: Int?
+    let health: Int?
+
+    var cardSet: MetadataItem?
+    var cardType: MetadataItem?
+    var rarity: MetadataItem?
+}
