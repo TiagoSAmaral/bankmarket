acd0c28244b1e50119d9f97dbfa978bce1d24aaa
Tiago Amaral <tiagofly@outlook.com>
2023-06-18 02:56:53 -0300

    Implementado Networkerror

---
+++ source/app/Shared/Workers/Network/NetworkError.swift
@@ -0,0 +1,44 @@
+//
+//  NetworkError.swift
+//  BaseProjectTarget
+//
+//  Created by Tiago Amaral on 18/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+
+    case offline(text: String)
+    case badRequest(text: String)
+    case notDefined(text: String)
+
+    var message: String {
+        switch self {
+        case .badRequest(let text), .offline(let text), .notDefined(text: let text):
+            return text
+        }
+    }
+
+    static func makeError(with status: Int?, description: String? = nil) -> NetworkError {
+        let messageNotDefinedError = LocalizedText.with(tagName: .networkErrorNotDefined)
+        guard let status = status else {
+            return .notDefined(text: messageNotDefinedError)
+        }
+        
+        if status == -1 {
+            return .offline(text: description ?? LocalizedText.with(tagName: .networkOffline))
+        }
+        
+        if 400 ... 499 ~= status {
+            return .badRequest(text: description ?? messageNotDefinedError)
+        }
+        
+        if status > 499 {
+            return .notDefined(text: description ?? LocalizedText.with(tagName: .serverNotResponse))
+        }
+        
+        return .notDefined(text: messageNotDefinedError)
+    }
+}
