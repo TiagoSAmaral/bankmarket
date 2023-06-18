d469a3834392cb5795b9ec47a9717accc00a0378
Tiago Amaral <tiagofly@outlook.com>
2023-06-18 03:05:44 -0300

    Update ClassIdentifier.swift Usado para identificar classes passando seu nome para string.

---
+++ source/app/Shared/Extensions/NSObject/ClassIdentifier.swift
@@ -0,0 +1,21 @@
+//
+//  ClassIdentifier.swift
+//  BaseProjectTarget
+//
+//  Created by Tiago Amaral on 18/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import Foundation
+    
+import Foundation
+
+protocol ClassIdentifier {}
+
+extension ClassIdentifier where Self: NSObject {
+    static var classIdentifier: String {
+        String(describing: self)
+    }
+}
+
+extension NSObject: ClassIdentifier {}
