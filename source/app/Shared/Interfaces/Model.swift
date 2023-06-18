52c229cb97c82f4b3dbd4f15dc6e5923c5e7ca57
Tiago Amaral <tiagofly@outlook.com>
2023-06-18 03:01:16 -0300

    Update Model.swift Implementado base de model para generalizar passagem de dados em qualquer fluxo necessário.

---
+++ source/app/Shared/Interfaces/Model.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  BaseProjectTarget
+//
+//  Created by Tiago Amaral on 17/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import Foundation
+
+protocol Model {
+    
+}
