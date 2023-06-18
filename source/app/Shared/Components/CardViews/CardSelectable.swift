2c11f4e6e0f407bdf59685b1d31a17aae4fc9067
Tiago Amaral <tiagofly@outlook.com>
2023-06-18 03:08:28 -0300

    Update CardSelectable.swift Classe base para qualquer UIView que será um card. Já tras uma implementação reusada para injetar acao vinda de model.

---
+++ source/app/Shared/Components/CardViews/CardSelectable.swift
@@ -0,0 +1,33 @@
+//
+//  CardSelectable.swift
+//  BaseProjectTarget
+//
+//  Created by Tiago Amaral on 18/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import UIKit
+
+class CardSelectable: UIView, CardVisible {
+    
+    var action: ((Model?) -> Void)?
+    var model: Model? {
+        willSet {
+            action = (newValue as? Selectable)?.actionOnTap
+            makeTapEvent()
+        }
+    }
+    
+    func load(model: Model?) {
+        fatalError("Must override this method")
+    }
+    
+    func makeTapEvent() {
+        let tapAction = UITapGestureRecognizer(target: self, action: #selector(actionWhenTap))
+        addGestureRecognizer(tapAction)
+    }
+    
+    @objc func actionWhenTap() {
+        action?(model)
+    }
+}
