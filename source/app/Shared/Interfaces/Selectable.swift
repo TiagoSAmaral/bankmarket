b542829d035b7eef60dd46f4eeaa62732c77fac4
Tiago Amaral <tiagofly@outlook.com>
2023-06-18 03:00:06 -0300

    Update Selectable.swift Implementado protocolo para items visíveis que possuirem evento de toque

---
+++ source/app/Shared/Interfaces/Selectable.swift
@@ -0,0 +1,14 @@
+//
+//  Selectable.swift
+//  BaseProjectTarget
+//
+//  Created by Tiago Amaral on 17/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import Foundation
+
+protocol Selectable where Self: Model {
+    
+    var actionOnTap: ((Model?) -> Void)? { get set }
+}
