a21362470196990e6fde821b2519e9fefb5b2390
Tiago Amaral <tiagofly@outlook.com>
2023-06-18 03:00:26 -0300

    Update TableViewUpdateEvent.swift Interface para atualizar lista/tableview

---
+++ source/app/Shared/Interfaces/TableViewUpdateEvent.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewUpdateEvent.swift
+//  BaseProjectTarget
+//
+//  Created by Tiago Amaral on 18/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import Foundation
+
+protocol TableViewUpdateEvent {
+    func reloadView()
+}
