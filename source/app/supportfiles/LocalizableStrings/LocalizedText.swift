51b059f73065a1bf6a89edb32c60b337315c517f
Tiago Amaral <tiagofly@outlook.com>
2023-06-18 02:56:39 -0300

    Update LocalizedText.swift Atualizado enum de mensagens fixas

--- source/app/supportfiles/LocalizableStrings/LocalizedText.swift
+++ source/app/supportfiles/LocalizableStrings/LocalizedText.swift
@@ -10,8 +10,11 @@ import Foundation
 struct LocalizedText {
     
     enum LocalizeTags: String {
-        case messageReadme
-    }
+            case networkErrorNotDefined // = "networkErrorNotDefined"
+            case networkOffline // = "networkOffline"
+            case serverNotResponse // = "serverNotResponse"
+            case pullToRefreshText
+        }
     
     static func with(tagName: LocalizeTags) -> String {
         return Bundle.main.localizedString(forKey: tagName.rawValue, value: nil, table: "Localizable-pt-BR")
