d581640b74d97f08d65e68f5e08199760aa0c823
Tiago Amaral <tiagofly@outlook.com>
2023-06-18 03:05:21 -0300

    Update HtmlAttributedString.swift Exensão apra tratar recebimento de strings com tags HTML.

---
+++ source/app/Shared/Extensions/String/HtmlAttributedString.swift
@@ -0,0 +1,23 @@
+//
+//  HtmlAttributedString.swift
+//  BaseProjectTarget
+//
+//  Created by Tiago Amaral on 18/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var htmlToAttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return nil }
+        do {
+            return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch {
+            return nil
+        }
+    }
+    var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+    }
+}
