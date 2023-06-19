a759394936ec33ad57cb7a685e1de8fea592d3ee
Tiago Amaral <tiagofly@outlook.com>
2023-06-19 00:26:36 -0300

    Update Empty.swift Adicionado extensão de String `empty` para reduzir uso direto de string vazia

---
+++ source/app/Shared/Extensions/String/Empty.swift
@@ -0,0 +1,16 @@
+//
+//  Empty.swift
+//  HeartOfStoneViewerTarget
+//
+//  Created by Tiago Amaral on 18/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    static var empty: String {
+        ""
+    }
+}
