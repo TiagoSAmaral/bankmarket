167898f173cd85e63ce245a5c3da4907041a1686
Tiago Amaral <tiagofly@outlook.com>
2023-06-19 18:05:33 -0300

    Update CardListItemView.swift Implementado layout de CardListItemView.

--- source/app/Shared/Components/CardViews/CardListItemView.swift
+++ source/app/Shared/Components/CardViews/CardListItemView.swift
@@ -7,9 +7,94 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class CardListItemView: CardSelectable {
     
+    let cornerRadius: CGFloat = 8.0
+    
+    lazy var baseView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = cornerRadius
+        view.clipsToBounds =  true
+        return view
+    }()
+
+    lazy var imageViewBackground: UIImageView = {
+       let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = cornerRadius
+        imageView.clipsToBounds =  true
+        return imageView
+    }()
+
+    lazy var vStackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.axis = .vertical
+        return stackView
+    }()
+
+    lazy var titleLabel: UILabel = {
+       let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 22, weight: .heavy)
+        label.textColor = .yellow
+        return label
+    }()
+
+    lazy var descriptionLabelBaseView: UIView = {
+       let view = UIView()
+        
+        return view
+    }()
+    
+    lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        label.textColor = .white
+        label.numberOfLines = .zero
+        return label
+    }()
+
     override func load(model: Model?) {
+        self.model = model
+        makeLayoutHorizontalStackView()
+        populateViewElements()
+        registerAction()
+    }
+    
+    func populateViewElements() {
+        let visibleModel = model as? ListItemModelVisible
+        
+        titleLabel.text = visibleModel?.title
+        descriptionLabel.text = visibleModel?.flavorDescription
+        if let imageUrl = visibleModel?.imageUrlPath {
+            imageViewBackground.kf.setImage(with: URL(string: imageUrl))
+        }
+    }
+    
+    func registerAction() {
+        let selectableModel = model as? Selectable
+        self.action = selectableModel?.actionOnTap
+    }
+    
+    func makeLayoutHorizontalStackView() {
+        addSubviews([baseView])
+        baseView.addSubviews([imageViewBackground, vStackView])
+        
+        imageViewBackground.edgeToSuperView()
+        vStackView.edgeToSuperView(margin: 8.0)
+        
+        descriptionLabelBaseView.addSubviews([descriptionLabel])
+        
+        descriptionLabel.centerX(of: descriptionLabelBaseView)
+        descriptionLabel.height(60)
+        vStackView.addArrangedSubview(titleLabel)
+        vStackView.addArrangedSubview(descriptionLabel)
+        
+        baseView.height(100.0)
+        baseView.edgeToSuperView(margin: 8.0)
+        imageViewBackground.edgeToSuperView()
+        
+        baseView.bringSubviewToFront(vStackView)
     }
 }
