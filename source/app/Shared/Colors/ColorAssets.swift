662b2ff149b15b945fa2f83eb44e888c4338fd5f
Tiago Amaral <tiagofly@outlook.com>
2023-06-19 18:23:49 -0300

    Criado paleta de cores para light e dark theme

---
+++ source/app/Shared/Colors/ColorAssets.swift
@@ -0,0 +1,15 @@
+//
+//  ColorAssets.swift
+//  HeartOfStoneViewerTarget
+//
+//  Created by Tiago Amaral on 19/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import UIKit
+
+struct ColorAssets {
+    static let titleColor = UIColor(named: "title")
+    static let descriptionColor = UIColor(named: "description")
+    static let cardBackgroundColor = UIColor(named: "background")
+}
---
+++ source/app/supportfiles/assets/ColorsAsset.swift
@@ -0,0 +1,9 @@
+//
+//  ColorsAsset.swift
+//  HeartOfStoneViewerTarget
+//
+//  Created by Tiago Amaral on 19/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import Foundation
