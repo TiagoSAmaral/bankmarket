732bfb93a5b7ab07abc3db5e817f1d4399a295f3
Tiago Amaral <tiagofly@outlook.com>
2023-06-19 00:27:49 -0300

    Update ListItemInteractor.swift Implementado requisicao de token e listagem de item.

--- source/app/Scenes/ListItemScene/ListItemInteractor.swift
+++ source/app/Scenes/ListItemScene/ListItemInteractor.swift
@@ -14,31 +14,68 @@ protocol ListItemInteractorBusinessLogic {
 }
 
 final class ListItemInteractor: ListItemInteractorBusinessLogic {
-   
+    
     var presenter: ListItemPresentationLogic?
-    var worker: NetworkWorker?
+    var workerNetwork: NetworkWorker?
+    var workerSecurity: SecurityWorker?
+    var workerApiPathBuilder: WorkerURLPathBuilder?
+    var currentLocale: String = "pt_BR"
     
     var lastResponse: ResponseList?
     
     func fetchItems() {
-    
-        worker?.request(with: ApiParams(page: nextPage()), resultType: ResponseList.self) { [weak self] response in
-            
-            switch response {
-            case .success(let data):
-                self?.lastResponse = data
-                self?.presenter?.presentList(with: data.cards)
-                break
+        
+        let requestGroup =  DispatchGroup()
+        var authToken: String?
+        
+        requestGroup.enter()
+        
+        workerSecurity?.getUserToken(handler: { [weak self] result in
+            switch result {
+            case .success(let token):
+                if let token = token {
+                    authToken = token
+                } else {
+                    self?.presenter?.message(string: "Não foi possível realizar a requisição.")
+                }
+                requestGroup.leave()
             case .failure(let error):
-                break
+                self?.presenter?.message(string: error.message)
+                requestGroup.leave()
             }
+        })
+        
+        requestGroup.notify(queue: DispatchQueue.main) { [weak self] in
+            if let authToken = authToken {
+                self?.requestItems(with: authToken, page: self?.nextPage())
+            } else {
+                self?.presenter?.message(string: "Não foi possível realizar a requisição.")
+            }
+        }
+    }
+    
+    func requestItems(with token: String?, page: Int?) {
+        
+        if let urlPath = workerApiPathBuilder?.makeUrlCards(with: nextPage(), locale: currentLocale), let token = token {
+
+            let apiParams = ApiParams(urlPath: urlPath, token: token, method: .get, params: nil)
+            
+            workerNetwork?.request(with: apiParams, resultType: ResponseList.self, handler: { [weak self] response in
+                switch response {
+                case .success(let responseList):
+                    self?.lastResponse = responseList
+                    self?.presenter?.presentList(with: responseList.cards)
+                case .failure(let error):
+                    self?.presenter?.message(string: error.message)
+                }
+            })
         }
     }
     
     func nextPage() -> Int {
         guard let lastResponse = lastResponse,
               let lastPage = lastResponse.page else {
-            return .zero
+            return 1
         }
         return lastPage + 1
     }
