f746c4f2d37e41ae9db89337350dea2d56ca6429
Tiago Amaral <tiagofly@outlook.com>
2023-06-19 19:08:41 -0300

    Update ColorsAsset.swift Removido arquivo não usado

--- source/app/supportfiles/assets/ColorsAsset.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ColorsAsset.swift
-//  HeartOfStoneViewerTarget
-//
-//  Created by Tiago Amaral on 19/06/23.
-//  Copyright © 2023 developerios. All rights reserved.
-//
-
-import Foundation
