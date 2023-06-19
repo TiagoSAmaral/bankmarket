34d406d83d4918de987d55e85b3390fe16649a79
Tiago Amaral <tiagofly@outlook.com>
2023-06-19 00:27:08 -0300

    Update ListItemSceneFactory.swift Implementado factory para tela de listar items

--- source/app/Scenes/ListItemScene/ListItemSceneFactory.swift
+++ source/app/Scenes/ListItemScene/ListItemSceneFactory.swift
@@ -11,6 +11,31 @@ import UIKit
 enum ListItemSceneFactory {
     
     static func makeScene(with data: Model?) -> UIViewController? {
-        UIViewController()
+        let controller = ListItemController()
+        let presenter = ListItemPresenter()
+        let router = ListItemRouter()
+        let interactor = ListItemInteractor()
+        let view = ListItemView()
+        
+        let workerNetwork = NetworkConactable()
+        let workerSecurity = Security()
+        workerSecurity.workerNetwork = NetworkConactable()
+        workerSecurity.workerUrlBuilder = URLPathBuilder()
+        workerSecurity.keyAdvisor = KeysAcessor()
+        let workerApiPathBuilder = URLPathBuilder()
+        
+        presenter.controller = controller
+        router.controller = controller
+        
+        interactor.presenter = presenter
+        interactor.workerNetwork = workerNetwork
+        interactor.workerSecurity = workerSecurity
+        interactor.workerApiPathBuilder = workerApiPathBuilder
+        
+        controller.interactor = interactor
+        controller.router = router
+        controller.view = view
+        
+        return controller
     }
 }
