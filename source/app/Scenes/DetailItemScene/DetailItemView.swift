173948d0247d1195875eeb8ab707f8b0232a607a
Tiago Amaral <tiagofly@outlook.com>
2023-06-20 01:58:38 -0300

    Update DetailItemRouter.swift, DetailItemView.swift, DetailItemViewModel.swift, and 3 more files Removido classes e arquivos não necessários.

--- source/app/Scenes/DetailItemScene/DetailItemView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ListView.swift
-//  BaseProjectTarget
-//
-//  Created by Tiago Amaral on 17/06/23.
-//  Copyright © 2023 developerios. All rights reserved.
-//
-
-import Foundation
--- source/app/Scenes/DetailItemScene/DetailItemViewModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ListViewModel.swift
-//  BaseProjectTarget
-//
-//  Created by Tiago Amaral on 17/06/23.
-//  Copyright © 2023 developerios. All rights reserved.
-//
-
-import Foundation
