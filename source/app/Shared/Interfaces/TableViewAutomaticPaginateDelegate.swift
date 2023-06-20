64723ec74256ca2bb9eee318e692790f2520e61a
Tiago Amaral <tiagofly@outlook.com>
2023-06-20 02:00:09 -0300

    Update TableViewAutomaticPaginateDelegate.swift Redefinido interface para TableViewAutomaticPagination

---
+++ source/app/Shared/Interfaces/TableViewAutomaticPaginateDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  TableViewAutomaticPaginateDelegate.swift
+//  hearthstonecardviewer
+//
+//  Created by Tiago Amaral on 20/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import Foundation
+
+protocol TableViewAutomaticPaginateDelegate: AnyObject {
+    func numberOfSections() -> Int
+    func numberOfRow(at section: Int) -> Int
+    func pullToRefreshEvent()
+    func nextPageEvent()
+    func getModel(at indexPath: IndexPath) -> Model?
+}
+
+extension TableViewAutomaticPaginateDelegate {
+    func pullToRefreshEvent() {}
+    func nextPageEvent() {}
+}
