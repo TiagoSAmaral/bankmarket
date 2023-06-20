857c3381cd00f6d2a525d23bed43df23481d7d1a
Tiago Amaral <tiagofly@outlook.com>
2023-06-19 23:40:12 -0300

    Update FontPallet.swift and LHF Uncial Caps.ttf adicionado fonte tema do jogo.

---
+++ source/app/supportfiles/font/FontPallet.swift
@@ -0,0 +1,13 @@
+//
+//  FontPallet.swift
+//  hearthstonecardviewer
+//
+//  Created by Tiago Amaral on 19/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import UIKit
+
+enum FontPallet {
+    static let mainFont = UIFont(name: "LHF Uncial Caps", size: 16)
+}
