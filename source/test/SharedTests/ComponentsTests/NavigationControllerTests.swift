3459e9721d659dc35bb0a484ae15cfba00d1c98e
Tiago Amaral <tiagofly@outlook.com>
2023-06-21 01:09:16 -0300

    Update project.pbxproj, testNavigationLayout.1.png, and NavigationControllerTests.swift

---
+++ source/test/SharedTests/ComponentsTests/NavigationControllerTests.swift
@@ -0,0 +1,23 @@
+//
+//  NavigationControllerTests.swift
+//  HeartOfStoneViewerTests
+//
+//  Created by Tiago Amaral on 21/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import SnapshotTesting
+import XCTest
+
+final class NavigationControllerTests: XCTestCase {
+
+    let isRecording = false
+    
+    func testNavigationLayout() {
+        let fakeViewController = UIViewController()
+        fakeViewController.title = "Test navigation"
+        let navigation = NavigationController()
+        navigation.viewControllers = [fakeViewController]
+        assertSnapshot(matching: navigation, as: .image, record: isRecording)
+    }
+}
