dc3ed62228b8da64d52a5151507b58569cbc9399
Tiago Amaral <tiagofly@outlook.com>
2023-06-21 02:27:14 -0300

    Update project.pbxproj, testNavigationLayout.1.png, and LoadingViewTests.swift implementado teste de loading.

---
+++ source/test/SharedTests/ComponentsTests/LoadingViewTests.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingViewTests.swift
+//  HeartOfStoneViewerTests
+//
+//  Created by Tiago Amaral on 21/06/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import SnapshotTesting
+import XCTest
+
+final class LoadingViewControllerTests: XCTestCase {
+    let isRecording = false
+    
+    func testNavigationLayout() {
+        let fakeViewController = LoadingViewController()
+        fakeViewController.title = "Test LoadingViewController"
+        let navigation = NavigationController()
+        navigation.viewControllers = [fakeViewController]
+        assertSnapshot(matching: navigation, as: .image, record: isRecording)
+    }
+}
