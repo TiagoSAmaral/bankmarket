9c8a5b8f1fb866c2f62cbfccb31f0e35efdbfd0e
Tiago Amaral <tiagofly@outlook.com>
2023-07-25 17:30:03 -0300

    Update Localizable.strings, LocalizedText.swift, and Localizable.strings Ajuste para texto fixo de apresentacao de ProfileView.

--- source/app/supportfiles/LocalizableStrings/LocalizedText.swift
+++ source/app/supportfiles/LocalizableStrings/LocalizedText.swift
@@ -23,6 +23,7 @@ final class LocalizedText {
             case healthText
             case urlAuthorizationNotFound
             case lastPage
+            case helloPresentation
         }
     static func with(tagName: LocalizeTags) -> String {
         return NSLocalizedString(tagName.rawValue, bundle: Bundle(for: LocalizedText.self), comment: "")
