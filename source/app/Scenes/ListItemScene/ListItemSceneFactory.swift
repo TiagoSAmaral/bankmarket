c3a2136837f9f7d549059ceecc7fa549bb1a48ca
Tiago Amaral <tiagofly@outlook.com>
2023-07-25 17:56:34 -0300

    Update ListItemSceneFactory.swift Ajuste da fabrica para adicionar profileview, e criado model fake para exibir item.

--- source/app/Scenes/ListItemScene/ListItemSceneFactory.swift
+++ source/app/Scenes/ListItemScene/ListItemSceneFactory.swift
@@ -15,12 +15,18 @@ enum ListItemSceneFactory {
         let router = ListItemRouter()
         let viewMosaic = MosaicBaseView()
         let listView = ListContructor().createVerticalList(with: presenter)
-
+        let profileViewer = ProfileView()
+                
+        let fakeProfileViewModel = ProfileFake.makeFake()
+        profileViewer.load(model: fakeProfileViewModel)
         listView.cardFactory = CardConstructor()
+        
+        viewMosaic.insertNew(view: profileViewer)
         viewMosaic.insertNew(view: listView)
 
         presenter.urlComposer = URLPathBuilder()
         presenter.network = NetworkConactable()
+        presenter.listItemAdapter = ListItemLayoutAdapter()
 
         presenter.controller = controller
         presenter.router = router
@@ -31,5 +37,14 @@ enum ListItemSceneFactory {
         controller.view = viewMosaic.baseView
 
         return controller
+    }
+}
+
+struct ProfileFake: Model, ProfileViewModel {
+    var titleValue: String?
+    var profileImage: String?
+    
+    static func makeFake() -> ProfileFake {
+        ProfileFake(titleValue: "Tiago Amaral", profileImage: "https://media.licdn.com/dms/image/C4D03AQEAEDnu83wgQA/profile-displayphoto-shrink_200_200/0/1625150055190?e=1695859200&v=beta&t=eze0cXqLYMyzcd5XfmYdOsl2Py86jhRBv0_GA2Zr3Sg")
     }
 }
