9b026daa31db34199c5ba2d6460141176670f9a3
Tiago Amaral <tiagofly@outlook.com>
2023-07-25 17:50:52 -0300

    Update ListItemViewModelMap.swift Criado map de dados para exibir tela de Detalhe.

---
+++ source/app/Shared/Models/ListItemViewModelMap.swift
@@ -0,0 +1,35 @@
+//
+//  ListItemViewModelMap.swift
+//  bankmarketTarget
+//
+//  Created by Tiago Amaral on 22/07/23.
+//  Copyright © 2023 developerios. All rights reserved.
+//
+
+import Foundation
+
+protocol ListItemViewModelMap: CardFullImageViewModel, CardDumpDetailViewModel, Selectable where Self: Visible {
+    var title: String? { get }
+    var bannerURL: String? { get }
+    var description: String? { get }
+}
+
+protocol ListItemResponseViewModelMap {
+    var spotlight: [Item]? { get }
+    var products: [Item]? { get }
+    var cash: Item? { get }
+}
+
+protocol ListItemViewModelVisible: Visible {
+    var layoutView: CardLayoutView? { get set }
+    var items: [ListItemViewModelMap]? { get set }
+    var title: String? { get set }
+}
+
+struct ListItemViewModelMapped: ListItemViewModelVisible, CardTitleViewModel {
+    var title: String?
+    var height: CGFloat?
+    var width: CGFloat?
+    var layoutView: CardLayoutView?
+    var items: [ListItemViewModelMap]?
+}
